0fbbe092c4fae9f58f1e0031c9c306906db2560a
Robbie <ecuadorx100@gmail.com>
2025-02-26 01:12:18 -0500

    First Changes

---
+++ crawler.py
@@ -0,0 +1,7 @@
+# Appointment requirement
+# Can be not set and all supported types will be checked
+APPOINTMENT_TYPES = {"RENEWAL: LICENSE OR NON-DRIVER ID"}
+# Can be empty and all locations will be checked
+LOCATION = {"BAYONNE", "NORTH BERGEN", "NEWARK", "WAYNE", "PATERSON", "ELIZABETH"}
+
+# Slack configuration. Needed when only --slack flag is set
