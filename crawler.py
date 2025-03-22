6cb90d5923ab9f91857985ead467200370ef68bc
Robbie <rob76c@gmail.com>
2025-03-21 20:54:17 -0400

    Finished updating crawler

--- crawler.py
+++ crawler.py
@@ -3,8 +3,3 @@
 APPOINTMENT_TYPES = {"RENEWAL: LICENSE OR NON-DRIVER ID"}
 # Can be empty and all locations will be checked
 LOCATION = {"BAYONNE", "NORTH BERGEN", "NEWARK", "WAYNE", "PATERSON", "ELIZABETH"}
-
-# Slack configuration. Needed when only --slack flag is set
-SLACK_BOT_TOKEN = ""
-SLACK_CHANNEL_ID = ""
-
